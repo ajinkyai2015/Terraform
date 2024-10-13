ed6dbe1381c41b7de7bc49bc6b2228bacf51d4e2
Ajinkya Indulkar <ajinkyaindulkar.005@gmail.com>
2024-10-13 15:54:56 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-# Remove the following duplicate output definitions
-# output "ec2_instance_ids" {
-#   value = module.ec2_instances.instance_ids
-# }
-
-# output "security_group_id" {
-#   value = module.security_group.security_group_id
-# }
