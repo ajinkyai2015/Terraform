1c9e61b12601de9478f7b0ff5ed45ea69c3af785
Ajinkya Indulkar <ajinkyaindulkar.005@gmail.com>
2024-10-13 15:54:34 -0500

    Delete modules directory

--- modules/ec2/outputs.tf
+++
@@ -1,4 +0,0 @@
-# Remove the following output from this file
-# output "instance_ids" {
-#   value = aws_instance.ec2_instance.*.id
-# }
--- modules/security_group/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group_id" {
-  description = "The security group ID"
-  value       = aws_security_group.app_sg.id
-}
